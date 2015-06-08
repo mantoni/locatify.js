67a6f9e20c7328c09f91eba6049f801cdca13b95
Maximilian Antoni <mail@maxantoni.de>
2015-06-08 18:07:58 +0200

    Extract location-tracker from geo-tales-mobile

---
+++ test/util/fake-location.js
@@ -0,0 +1,44 @@
+/*
+ * geo-tales-mobile
+ *
+ * Copyright (c) 2015 Maximilian Antoni <mail@maxantoni.de>
+ *
+ * @license MIT
+ */
+/*global document, navigator*/
+'use strict';
+
+var sinon = require('sinon');
+
+if (!navigator.geolocation) {
+  var noop = function () { return; };
+  // PhantomJS doesn't support geolocation.
+  navigator.geolocation = {
+    watchPosition: noop,
+    clearWatch: noop
+  };
+}
+
+exports.create = function () {
+  var watch = sinon.stub(navigator.geolocation, 'watchPosition');
+  var clear = sinon.stub(navigator.geolocation, 'clearWatch');
+
+  return {
+
+    updateOrientation: function (props) {
+      var event = document.createEvent('HTMLEvents');
+      event.initEvent('deviceorientation', true, true);
+      if (props) {
+        Object.keys(props).forEach(function (key) {
+          event[key] = props[key];
+        });
+      }
+      document.dispatchEvent(event);
+    },
+
+    destroy: function () {
+      watch.restore();
+      clear.restore();
+    }
+  };
+};
